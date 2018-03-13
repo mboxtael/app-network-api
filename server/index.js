82a5c0b3ccb61e7e21367b79de030ed7a25fb3bb
Mauricio Boxtael <mboxtael@gmail.com>
2018-03-13 16:12:37 -0400

    Added cors

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 const Koa = require('koa');
 const bodyParser = require('koa-bodyparser');
+const cors = require('@koa/cors');
 require('dotenv').config();
 
 const app = new Koa();
 app.use(bodyParser());
+app.use(cors());
 app.use((ctx, next) =>
   next().catch(err => {
     if (err.status === 401) {
