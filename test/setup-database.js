dc48645f3bd0e377a8c39ef97a3d2c0d4a8ee6ef
Mauricio Boxtael <mboxtael@gmail.com>
2018-03-18 20:38:12 -0400

    Removed comments

--- test/setup-database.js
+++ test/setup-database.js
@@ -1,38 +1,29 @@
 const mongoose = require('mongoose');
-// const MongodbMemoryServer = require('mongodb-memory-server').default;
 
+const MONGO_URI = 'mongodb://mongo/networking_test';
 const config = { connection: null };
 mongoose.Promise = Promise;
 
-function connect() {
-  return new Promise((resolve, reject) => {
-    if (config.connection) {
-      return resolve();
+async function connect() {
+  if (config.connection) {
+    return;
+  }
+
+  const mongooseOpts = {
+    autoReconnect: true,
+    reconnectTries: Number.MAX_VALUE,
+    reconnectInterval: 1000
+  };
+
+  await mongoose.connect(MONGO_URI, mongooseOpts);
+  config.connection = mongoose.connection;
+
+  mongoose.connection.on('error', async e => {
+    if (e.message.code === 'ETIMEDOUT') {
+      await mongoose.connect(MONGO_URI, mongooseOpts);
     }
 
-    //const mongoServer = new MongodbMemoryServer();
-
-    //mongoServer.getConnectionString().then(mongoUri => {
-    const mongooseOpts = {
-      autoReconnect: true,
-      reconnectTries: Number.MAX_VALUE,
-      reconnectInterval: 1000
-    };
-
-    mongoose.connect('mongodb://mongo/networking_test', mongooseOpts);
-    config.connection = mongoose.connection;
-
-    mongoose.connection.on('error', e => {
-      if (e.message.code === 'ETIMEDOUT') {
-        console.log(e);
-        mongoose.connect(mongoUri, mongooseOpts);
-      }
-      console.log(e);
-      reject(e);
-    });
-
-    mongoose.connection.once('open', resolve);
-    //});
+    throw e;
   });
 }
 
